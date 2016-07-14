b1124c7500e27f2ae3fb8c71fabcfab0db4d0443
Caroline Joseph <carbroj@gmail.com>
2016-07-13 20:54:40 -0500

    added model methods for tax and total

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,2 +1,23 @@
 class Sock < ActiveRecord::Base
+  def friendly_created_at
+    created_at.strftime('%b %d, %Y %l:%M %p')
+    # strftime
+  end 
+
+  def sale_message
+    if price.to_i < 2
+      "Discount Item!"
+    else
+      "On Sale!"
+    end
+  end
+
+  def tax
+    @tax = price.to_i * 0.09
+    @tax
+  end
+
+  def total
+    price.to_i + @tax
+  end
 end
